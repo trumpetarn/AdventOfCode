39b8727785f271594a362ed0e17cef41a2d002d8
Jakob Markgren <markgren@outlook.com>
2018-12-02 16:31:53 +0100

    [2018:Day1] Second task and filemanagement

--- 2018/cpp/Day1_ChronicalCalibration.cpp
+++ 2018/cpp/Day1_ChronicalCalibration.cpp
@@ -2,8 +2,13 @@
 #include <fstream>
 #include <cstdint>
 #include <string>
+#include <set>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+vector<int> f_change;
 
 int part_one() {
 	string line;
@@ -14,9 +19,24 @@ int part_one() {
   {
     while ( getline (input,line) )
     {
+    	f_change.push_back(stoi(line));
       freq += stoi(line);
     }
     input.close();
+  }
+	return freq;
+}
+
+int part_two() {
+	string line;
+	int freq = 0;
+	set<int> freqs;
+	int i = 0;
+  while (!(freqs.find(freq) != freqs.end()))
+  {
+  	freqs.insert(freq);
+    freq += f_change[i];
+    i = (i+1)%f_change.size();
   }
 	return freq;
 }
@@ -25,6 +45,7 @@ int main(int, char**)
 {
 	int result = part_one();
 	cout << "Part 1: Frequency: " << result << "\n";
-
+	result = part_two();
+	cout << "Part 2: Frequency: " << result << "\n";
 	return 0;
 }
