cb406858d8b97c050254eba41744971bb70cbf68
Jakob Markgren <markgren@outlook.com>
2018-12-04 22:42:00 +0100

    [2018:Day4] Added files, no solution

---
+++ 2018/cpp/Day04_ReposeRecord.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main (int, char**)
+{
+	return 0;
+}
