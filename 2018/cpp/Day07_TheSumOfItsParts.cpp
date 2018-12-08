87fbd68f2a81c268456ea4532380662af0a843ce
Jakob Markgren <markgren@outlook.com>
2018-12-08 13:02:50 +0100

    [2018:Day7] Day 7 part 1

--- 2018/cpp/Day07_TheSumOfItsParts.cpp
+++ 2018/cpp/Day07_TheSumOfItsParts.cpp
@@ -12,6 +12,14 @@
 
 using namespace std;
 
+struct worker
+{
+	int id;
+	bool is_working;
+	char task;
+	int time_done;
+};
+
 vector<string> read_input(string filename)
 {
 	vector<string> in;
@@ -50,20 +58,78 @@ map<char,vector<char>> parse_input(vector<string> in)
 	return parsed;
 }
 
+bool has_no_dependecy(char c, map<char,vector<char>> instr, vector<char> skip)
+{
+	bool no_dependency = true;
+	for (auto it = instr.begin(); it != instr.end(); ++it)
+	{
+		if (find(skip.begin(),skip.end(), it->first) == skip.end())
+		{
+			for (auto it2 = it->second.begin(); it2 != it->second.end(); ++it2)
+			{
+				no_dependency = no_dependency && (c != (*it2));
+			}
+		}
+	}
+	return no_dependency;
+}
+
+vector<char> part_one(map<char,vector<char>> instr, vector<char> start_candidates)
+{
+	vector<char> to_do;
+	vector<char> candidates = start_candidates;
+	while (to_do.size() < instr.size())
+	{
+		sort(candidates.begin(), candidates.end());
+		char last = candidates[0];
+		candidates.erase(candidates.begin());
+		to_do.push_back(last);
+		for (auto it2 = instr[last].begin(); it2!=instr[last].end();++it2)
+		{
+			if(has_no_dependecy(*it2, instr, to_do))
+				candidates.push_back(*it2);
+		}
+	}
+	return to_do;
+}
+
+vector<char> part_two(map<char,vector<char>> instr, vector<char> start_candidates)
+{
+	vector<char> to_do;
+	vector<char> candidates = start_candidates;
+	while (to_do.size() < instr.size())
+	{
+		sort(candidates.begin(), candidates.end());
+		char last = candidates[0];
+		candidates.erase(candidates.begin());
+		to_do.push_back(last);
+		for (auto it2 = instr[last].begin(); it2!=instr[last].end();++it2)
+		{
+			if(has_no_dependecy(*it2, instr, to_do))
+				candidates.push_back(*it2);
+		}
+	}
+	return to_do;
+}
+
+
 int main(int, char**)
 {
 	vector<string> in = read_input("../inputs/day07.txt");
 	map<char,vector<char>> instr = parse_input(in);
-	stack<char> to_do;
+	vector<char> to_do;
+	vector<char> candidates;
+	bool no_dependency;
 	for (auto it = instr.begin(); it != instr.end(); ++it)
 	{
-		to_do.push();
-	}
-	cout << "Task 1:";
-	while (!to_do.empty()) {
-		cout<<to_do.top();
-		to_do.pop();
-	}
+		no_dependency = has_no_dependecy(it->first, instr, {'0'});
+		if (no_dependency)
+			candidates.push_back(it->first);		
+	}	
+	to_do = part_one(instr, candidates);
+	cout << "Task 1: ";
+	for (auto i: to_do)
+		cout << i;
 	cout << endl;
 	return 0;
 }
