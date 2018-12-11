a6057deac5af1ce4b8ac7a3094e6dd1c2f8c76eb
Jakob Markgren <markgren@outlook.com>
2018-12-11 22:14:44 +0100

    [2018:Day10] Template added and Day 10 started

---
+++ 2018/cpp/dayXX.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <numeric>
+#include <unistd.h>
+#include <iterator>
+#include <list>
+
+using namespace std;
+
+vector<string> read_input(string loc)
+{
+	vector<string> in;
+	string line;
+	ifstream infile (loc);
+	if (infile.is_open())
+	{
+		while (getline(infile,line))
+		{
+			in.push_back(line);
+		}
+	}
+	return in;
+}
+
+int main(int argc, char** argv)
+{
+	string loc = "../inputs/dayXX.txt";
+	if (argc >= 2)
+		loc = argv[1];
+
+	vector<string> in = read_input(loc);
+	return 0;
+}
