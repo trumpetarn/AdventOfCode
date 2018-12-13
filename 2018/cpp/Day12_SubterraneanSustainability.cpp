dc61369cb1c6d5917bba848697ac4fd3a16bb6bc
Jakob Markgren <markgren@outlook.com>
2018-12-13 21:19:01 +0100

    [2018:Day12] Day 12 done

--- 2018/cpp/Day12_SubterraneanSustainability.cpp
+++ 2018/cpp/Day12_SubterraneanSustainability.cpp
@@ -38,38 +38,66 @@ int main(int argc, char** argv)
 		loc = argv[1];
 
 	vector<string> in = read_input(loc);
-	string initial = in[0].substr(15);
+	string initial = "....................";
+	initial += in[0].substr(15);
+	initial += "........................................";
+	
+	int displacement = 0;
+	int displacement20 = 0;
 	map<string,char> change_map;
 	for (int i=2; i<(int)in.size(); i++)
 	{
 		change_map[in[i].substr(0,5)] = in[i][9];
 	}
-	for (int gen=0; gen<20; gen++)
+	list<string> hist;
+	string gen20;
+	for (int gen=0; gen<500000; gen++)
 	{
 		string next_gen = initial;
-		string temp = ".." + initial.substr(0,3);
-		next_gen[0] = change_map[temp];
-		temp = "." + initial.substr(0,4);
-		next_gen[1] = change_map[temp];
-		int len = (int)initial.size()-2;
-		for (int i=2; i<len; i++)
+		for (int i=2; i<(int)initial.size()-2; i++)
 		{
-			temp = initial.substr(i-2,5);
+			//initial = initial + ".....";
+			string temp = initial.substr(i-2,5);
 			next_gen[i] = change_map[temp];
 		}
-		temp = initial.substr(len-4,4) + ".";
-		next_gen[len-1] = change_map[temp];
-		temp = initial.substr(len-3,3) + "..";
-		next_gen[len] = change_map[temp];
+		auto first_hash = next_gen.find('#');
+		if (first_hash > 10)
+			first_hash -= 5;
+		else
+			first_hash = 0;
+		displacement += (int) first_hash;
+		next_gen.erase(0,first_hash);
+		//cout << first_hash<< ','<< displacement << ':' << next_gen.size() << endl;
+		next_gen.append(first_hash,'.');
+		//cout << next_gen << endl;
+		//cout << next_gen << endl;
+		//cout << first_hash << ':' << next_gen.size() << ':' << initial.size() << endl;
 		initial = next_gen;
-		cout << initial << endl;
+		if (gen==19)
+		{
+			gen20 = initial;
+			displacement20 = displacement;
+		}
+		//cout << initial << endl << endl;
+		if (find(hist.begin(), hist.end(), initial) != hist.end())
+		{
+			cout << gen << ": Recurrence " << *(find(hist.begin(), hist.end(), initial)) << endl;
+			break;
+		}
+		hist.push_back(initial);
 	}
-	int ans = 0;
+	long long int ans1 = 0;
+	long long int ans2 = 0;
 	for (int i=0; i<(int)initial.size(); i++)
 	{
+		if (gen20[i] == '#')
+			ans1 += i-20+displacement20;
 		if (initial[i] == '#')
-			ans += i-20;
+			ans2 += i-20+displacement+(50000000000-hist.size()-1);
 	}	
-	cout << "Task 1: " << ans << endl;
+
+	cout << "Task 1: " << ans1 << endl;
+	cout << "Task 2: " << ans2 << endl;
+
 	return 0;
 }
