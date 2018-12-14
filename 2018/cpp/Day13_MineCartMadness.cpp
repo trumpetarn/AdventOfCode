631191a3271869f0ed6813e5f698e49b1d7bb690
Jakob Markgren <markgren@outlook.com>
2018-12-14 19:02:53 +0100

    [2018:Day13] Second star

--- 2018/cpp/Day13_MineCartMadness.cpp
+++ 2018/cpp/Day13_MineCartMadness.cpp
@@ -28,19 +28,17 @@ enum dir_t
 	RIGHT,
 	UP
 };
-/*
-struct coord_t
-{
-	int x;
-	int y;
-};
-*/
+
 struct cart_t
 {
 	dir_t direction;
 	turn_t next_turn;
 	char current_sym;
 	int last_tick;
+	bool has_crashed;
+	bool is_dead;
+	int x;
+	int y;
 };
 
 vector<string> read_input(string loc)
@@ -77,25 +75,25 @@ int main(int argc, char** argv)
 			if (in[k][l]=='v')
 			{
 				mine_map[k][l] = cart + 'A';
-				carts.push_back({DOWN,L,'|',0});
+				carts.push_back({DOWN,L,'|',0,false,false,0,0});
 				cart++;
 			}
 			else if (in[k][l]=='<')
 			{
 				mine_map[k][l] = cart + 'A';
-				carts.push_back({LEFT,L,'-',0});
+				carts.push_back({LEFT,L,'-',0,false,false,0,0});
 				cart++;
 			}
 			else if (in[k][l]=='>')
 			{
 				mine_map[k][l] = cart + 'A';
-				carts.push_back({RIGHT,L,'-',0});
+				carts.push_back({RIGHT,L,'-',0,false,false,0,0});
 				cart++;
 			}
 			else if (in[k][l]=='^')
 			{
 				mine_map[k][l] = cart + 'A';
-				carts.push_back({UP,L,'|',0});
+				carts.push_back({UP,L,'|',0,false,false,0,0});
 				cart++;
 			}
 			else
@@ -104,10 +102,21 @@ int main(int argc, char** argv)
 			}
 		}
 	}
-	bool has_crashed = false;
 	int tick = 0;
 	int num_carts = (int)carts.size();
-	while (num_carts > 0)
+	cout << num_carts << endl;
+	if (debug)
+	{
+		for (int k=0; k<i; ++k)
+		{
+			for (int l=0; l<j; ++l)
+			{
+				cout << mine_map[k][l];
+			}
+			cout << endl;
+		}
+	}
+	while (num_carts > 1)
 	{
 		tick++;
 		for (int k=0; k<i; ++k)
@@ -117,7 +126,7 @@ int main(int argc, char** argv)
 				if (mine_map[k][l]>='A' && mine_map[k][l]<'A'+cart)
 				{
 					int index = mine_map[k][l]-'A';
-					if (carts[index].last_tick<tick)
+					if (carts[index].last_tick<tick && !carts[index].is_dead)
 					{
 						if (carts[index].direction == LEFT)
 						{
@@ -137,12 +146,14 @@ int main(int argc, char** argv)
 									carts[index].direction = UP;
 									carts[index].next_turn = L;
 								}
-							}else if (next != '-'){
-								cout << next << ": ";
+							}else if (next != '-' && !carts[index].has_crashed){
 								cout << "Crash at: " << l-1 << ',' << k << endl;
-								has_crashed = true;
-								break;
+								carts[index].has_crashed=true;
+								carts[next-'A'].has_crashed=true;
+								mine_map[k][l] = carts[next-'A'].current_sym;
 							}
+							carts[index].x = l-1;
+							carts[index].y = k;
 							mine_map[k][l-1] = mine_map[k][l];
 							mine_map[k][l] = carts[index].current_sym;
 							carts[index].current_sym = next;
@@ -163,12 +174,14 @@ int main(int argc, char** argv)
 									carts[index].direction = DOWN;
 									carts[index].next_turn = L;
 								}
-							}else if (next != '-'){
-								cout << next << "; ";
+							}else if (next != '-' && !carts[index].has_crashed){
 								cout << "Crash at: " << l+1 << ',' << k << endl;
-								has_crashed = true;
-								break;
+								carts[index].has_crashed=true;
+								carts[next-'A'].has_crashed=true;
+								mine_map[k][l] = carts[next-'A'].current_sym;
 							}
+							carts[index].x = l+1;
+							carts[index].y = k;
 							mine_map[k][l+1] = mine_map[k][l];
 							mine_map[k][l] = carts[index].current_sym;
 							carts[index].current_sym = next;
@@ -189,12 +202,14 @@ int main(int argc, char** argv)
 									carts[index].direction = RIGHT;
 									carts[index].next_turn = L;
 								}
-							}else if (next != '|'){
-								cout << next << ", ";
+							}else if (next != '|' && !carts[index].has_crashed){
 								cout << "Crash at: " << l << ',' << k-1 << endl;
-								has_crashed = true;
-								break;
+								carts[index].has_crashed=true;
+								carts[next-'A'].has_crashed=true;
+								mine_map[k][l] = carts[next-'A'].current_sym;
 							}
+							carts[index].x = l;
+							carts[index].y = k-1;
 							mine_map[k-1][l] = mine_map[k][l];
 							mine_map[k][l] = carts[index].current_sym;
 							carts[index].current_sym = next;
@@ -215,12 +230,14 @@ int main(int argc, char** argv)
 									carts[index].direction = LEFT;
 									carts[index].next_turn = L;
 								}
-							}else if (next != '|'){
-								cout << next << "= ";
+							}else if (next != '|' && !carts[index].has_crashed){
 								cout << "Crash at: " << l << ',' << k+1 << endl;
-								has_crashed = true;
-								break;
+								carts[index].has_crashed=true;
+								carts[next-'A'].has_crashed=true;
+								mine_map[k][l] = carts[next-'A'].current_sym;
 							}
+							carts[index].x = l;
+							carts[index].y = k+1;
 							mine_map[k+1][l] = mine_map[k][l];
 							mine_map[k][l] = carts[index].current_sym;
 							carts[index].current_sym = next;
@@ -233,10 +250,19 @@ int main(int argc, char** argv)
 					carts[index].last_tick = tick;
 				}
 			}
-			if(has_crashed)
-				break;
 		}
-	if (debug)
+		for (auto it=carts.begin(); it!=carts.end(); ++it)
+		{
+			if (it->has_crashed)
+			{
+				//cout << "Crash: " << tick << endl;
+				it->has_crashed=false;
+				it->is_dead=true;
+				//mine_map[it->y][it->x] = it->current_sym;
+				num_carts--;
+			}
+		}
+		if (debug)
 		{
 			for (int k=0; k<i; ++k)
 			{
@@ -246,6 +272,13 @@ int main(int argc, char** argv)
 				}
 				cout << endl;
 			}
+		}
+	}
+	for (auto it=carts.begin(); it!=carts.end(); ++it)
+	{
+		if (!it->has_crashed && !it->is_dead)
+		{
+				cout << "Task 2: " << it->x << ',' << it->y << endl;
 		}
 	}
 	return 0;
