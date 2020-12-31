17b452688d28a42a2a19ae13410a0b9b5edb04ef
Jakob Markgren <markgren@outlook.com>
2020-12-31 18:46:01 +0100

    [2020:Day23] Hashmap is the way

--- 2020/go/day23.go
+++ 2020/go/day23.go
@@ -8,6 +8,7 @@ https://adventofcode.com/2020/day/23
 import (
 	"flag"
 	"fmt"
+	"container/list"
 	//"io/ioutil"
 	//"strings"
 	"strconv"
@@ -16,155 +17,143 @@ import (
 var input = flag.String("input", "598162734", "Input")
 var star = flag.Int("star", 0, "Which star to run (0=both)")
 
+
+func printList(l *list.List, i int, pick []int, d int) {
+	fmt.Println("-- Move", i, "--")
+	fmt.Print("Cups: ")
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Print(e.Value)
+	}
+	fmt.Println("")
+	fmt.Print("Pick Up: ")
+	for _,v := range pick {
+		fmt.Print(v)
+	}
+	fmt.Println("")
+	fmt.Println("Destination:", d)
+}
+
+func findValue(l *list.List, le *list.Element, val int) *list.Element {
+	fwd := le.Next()
+	bck := le.Prev()
+	for fwd != le {
+		
+		if fwd == nil {
+			fwd = l.Front()
+		}
+		if bck == nil {
+			bck = l.Back()
+		}
+
+		if fwd.Value.(int) == val {
+			return fwd
+		}
+		if bck.Value.(int) == val {
+			return bck
+		}
+		fwd = fwd.Next()
+		bck = bck.Prev()
+	}
+	return nil
+}
+
+func crabCups(data []int, iter int) (*list.List, *list.Element) {
+	cups := list.New()
+	valMap := make(map[int]*list.Element)
+	max := -1
+	for _,v := range data {
+		valMap[v] = cups.PushBack(v)
+		if v > max {
+			max = v
+		}
+	}
+	data = data[:1]
+	//printList(cups, 0, nil, 0)
+	current := cups.Front()
+	for i:=0;i<iter;i++ {
+		s := i%1000000
+		if s >= 0 {//s < 5000 || s>995000 {
+			cvalue := current.Value.(int)
+			dvalue := cvalue-1
+			if dvalue < 1 {
+				dvalue = max - dvalue
+			}
+
+			picked := make([]int, 3)
+			for j:=0;j<3;j++ {
+				picks := current.Next()
+				if picks == nil {
+					picks = cups.Front()
+				}
+				picked[j] = cups.Remove(picks).(int)
+			}
+
+			for k:=0;k<3;k++ {
+				for j:=0;j<3;j++ {
+					if dvalue == picked[j] {
+						dvalue--
+						if dvalue < 1 {
+							dvalue = max - dvalue
+						}
+					}
+				}
+			}
+
+			//printList(cups, i+1, picked, dvalue)
+			dest := valMap[dvalue]
+			for _,v := range picked {
+				dest = cups.InsertAfter(v, dest)
+				valMap[v] = dest
+			}
+		}
+
+		current = current.Next()
+		if current == nil {
+			current = cups.Front()
+		}
+	}
+	return cups, valMap[1]
+}
+
 func star1(data string) {
 	cups := make([]int, len(data))
 	for i,r := range data {
 		cups[i],_ = strconv.Atoi(string(r))
 	}
-	var current,dest int
-	var jC, jD, j1, j2 int
-	for i:=0;i<100;i++ {
-		if len(cups) == 0 {
-			panic("Zero")
-		}
-		jC = i % len(cups)
-		j1 = (i+1) % len(cups)
-		j2 = (i+4) % len(cups) 
-		current=cups[jC]
-
-		//fmt.Println("-- move", i+1, "--")
-		//fmt.Println("cups:", cups, current)
-		// Rule 1
-		pickedUp := make([]int, 3)
-		if j1 < j2 {
-			copy(pickedUp, cups[j1:j2])
-			cups = append(cups[:j1], cups[j2:]...)
-		} else {
-			copy(pickedUp, append(cups[j1:], cups[:j2]...))
-			cups = cups[j2:j1]
-		}
-		// Rule 2
-		dest = current
-		match := false
-		for !match {
-			dest--
-			if dest < 1 {
-				dest = 9
-			}
-			for k,v := range cups {
-				if v == dest {
-					match = true
-					jD = k
-					break
-				}
-			}
-		}
-		//fmt.Println("pick up:", pickedUp)
-		//fmt.Println("destination:", cups[jD])
-		// Rule 3
-		if jD+1<len(cups) {
-			pickedUp = append(pickedUp, cups[jD+1:]...)
-		}
-		cups = append(cups[:jD+1], pickedUp...)
-
-		if jD < jC {
-			for k:=0; k<3 && k<len(cups[jC:])-1; k++ {
-				tmp := cups[0]
-				cups = append(cups[1:], tmp)
-			}
-		}
+	l,a := crabCups(cups, 100)
+	a = a.Next()
+	if a == nil {
+		a = l.Front()
 	}
+
 	ans := ""
-	ind:=0
-	for k,v := range cups {
-		if v == 1 {
-			ind=k
-			break
+	for a.Value.(int) != 1 { 
+		ans += strconv.Itoa(a.Value.(int))
+		a = a.Next()
+		if a == nil {
+			a = l.Front()
 		}
-	}
-	for i:=0;i<len(cups)-1;i++ { 
-		ind++
-		ind = ind % len(cups)
-		ans += strconv.Itoa(cups[ind])
 	}
 	fmt.Println("Star 1:", ans)
 }
 
 func star2(data string) {
-	M := 1000000
-	//N := 500
 	cups := make([]int, len(data))
 	for i,r := range data {
 		cups[i],_ = strconv.Atoi(string(r))
 	}
-	for i:=10;i<=M;i++{
+	for i:=10;i<=1000000;i++ {
 		cups = append(cups, i)
 	}
-	
+	_,a := crabCups(cups, 10000000)
 
-	var current,dest int
-	var jC, jD, j1, j2 int
-	for i:=0;i<500;i++ {
-		jC = i % len(cups)
-		/*if (jC>=N) && (jC<=(10*M-N)) {
-			continue
-		}*/
-		j1 = (i+1) % len(cups)
-		j2 = (i+4) % len(cups) 
-		current=cups[jC]
-
-		//fmt.Println("-- move", i+1, "--")
-		//fmt.Println("cups:", cups, current)
-		// Rule 1
-		pickedUp := make([]int, 3)
-		if j1 < j2 {
-			copy(pickedUp, cups[j1:j2])
-			cups = append(cups[:j1], cups[j2:]...)
-		} else {
-			copy(pickedUp, append(cups[j1:], cups[:j2]...))
-			cups = cups[j2:j1]
-		}
-		// Rule 2
-		dest = current
-		match := false
-		for !match {
-			dest--
-			if dest < 1 {
-				dest = 9
-			}
-			for k,v := range cups {
-				if v == dest {
-					match = true
-					jD = k
-					break
-				}
-			}
-		}
-		//fmt.Println("pick up:", pickedUp)
-		//fmt.Println("destination:", cups[jD])
-		// Rule 3
-		if jD+1<len(cups) {
-			pickedUp = append(pickedUp, cups[jD+1:]...)
-		}
-		cups = append(cups[:jD+1], pickedUp...)
-
-		if jD < jC {
-			for k:=0; k<3 && k<len(cups[jC:])-1; k++ {
-				tmp := cups[0]
-				cups = append(cups[1:], tmp)
-			}
-		}
+	a = a.Next()
+	res := 1
+	for f:=0;f<2;f++ {
+		res *= a.Value.(int)
+		a = a.Next()
 	}
-
-	ind:=0
-	for k,v := range cups {
-		if v == 1 {
-			ind=k
-			break
-		}
-	}
-	fmt.Println(cups[ind-10:ind+10], ind)
-	fmt.Println("Star 2:", cups[ind+1]*cups[ind+2])
+	fmt.Println("Star 2:", res)
 }
 
 func main() {
