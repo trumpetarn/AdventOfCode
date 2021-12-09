005cc8242e00ba1fc9b7b61121b26bc3a8fd0aba
Jakob Markgren <markgren@outlook.com>
2021-12-09 23:16:34 +0100

    [2021:Day08,09] Day 8 and 9 solved

---
+++ 2021/go/day08.go
@@ -0,0 +1,150 @@
+package main
+
+/*
+Day 08: Title
+
+https://adventofcode.com/2020/day/8
+*/
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"sort"
+	"strings"
+)
+
+var inputFile = flag.String("input", "../inputs/day08.txt", "Relative path to input-file")
+
+func star1(data []string) {
+	ans := 0
+	for _, s := range data {
+		split := strings.Split(s, " ")
+		out := split[11:]
+		for _, o := range out {
+			n := len(o)
+			if n == 2 {
+				ans++
+			} else if n == 3 {
+				ans++
+			} else if n == 4 {
+				ans++
+			} else if n == 7 {
+				ans++
+			} else {
+				// nothing
+			}
+		}
+	}
+	fmt.Println("Star 1:", ans)
+}
+
+func star2(data []string) {
+	ans := 0
+	for _, s := range data {
+		var zero, one, two, three, four, five, six, seven, eight, nine string
+		var pos235, pos069 []string
+		split := strings.Split(s, " ")
+		usp := split[:10]
+		out := split[11:]
+		for _, o := range usp {
+			tmp := strings.Split(o, "")
+			sort.Strings(tmp)
+			o = strings.Join(tmp, "")
+			n := len(o)
+			if n == 2 && one == "" {
+				one = o
+			} else if n == 3 && seven == "" {
+				seven = o
+			} else if n == 4 && four == "" {
+				four = o
+			} else if n == 7 && eight == "" {
+				eight = o
+			} else if n == 5 {
+				// 2, 3, 5
+				pos235 = append(pos235, o)
+			} else if n == 6 {
+				// 0, 6, 9
+				pos069 = append(pos069, o)
+			}
+		}
+		//fmt.Println(pos069, pos235)
+		for _, p := range pos069 {
+			if strings.Contains(p, string(one[0])) && strings.Contains(p, string(one[1])) {
+				if strings.Contains(p, string(four[0])) && strings.Contains(p, string(four[1])) &&
+					strings.Contains(p, string(four[2])) && strings.Contains(p, string(four[3])) {
+					nine = p
+				} else {
+					zero = p
+				}
+			} else {
+				six = p
+			}
+			if zero != "" && nine != "" && six != "" {
+				break
+			}
+		}
+
+		for _, p := range pos235 {
+			if strings.Contains(p, string(one[0])) && strings.Contains(p, string(one[1])) {
+				three = p
+			} else {
+				if strings.Contains(six, string(p[0])) && strings.Contains(six, string(p[1])) &&
+					strings.Contains(six, string(p[2])) && strings.Contains(six, string(p[3])) &&
+					strings.Contains(six, string(p[4])) {
+					five = p
+				} else {
+					two = p
+				}
+			}
+			if two != "" && three != "" && five != "" {
+				break
+			}
+		}
+		n := len(out)
+		for i, o := range out {
+			tmp := strings.Split(o, "")
+			sort.Strings(tmp)
+			o = strings.Join(tmp, "")
+			ten := int(math.Pow10(n - i - 1))
+			switch o {
+			case zero:
+				ans += 0
+			case one:
+				ans += 1 * ten
+			case two:
+				ans += 2 * ten
+			case three:
+				ans += 3 * ten
+			case four:
+				ans += 4 * ten
+			case five:
+				ans += 5 * ten
+			case six:
+				ans += 6 * ten
+			case seven:
+				ans += 7 * ten
+			case eight:
+				ans += 8 * ten
+			case nine:
+				ans += 9 * ten
+			default:
+				fmt.Println(o)
+			}
+		}
+	}
+	fmt.Println("Star 2:", ans)
+}
+
+func main() {
+	flag.Parse()
+	raw, err := ioutil.ReadFile(*inputFile)
+	if err != nil {
+		panic("Invalid file")
+	}
+	rawStr := strings.TrimSpace(string(raw))
+	data := strings.Split(rawStr, "\n")
+	star1(data)
+	star2(data)
+}
---
+++ 2021/go/day09.go
@@ -0,0 +1,164 @@
+package main
+
+/*
+Day 09: Title
+
+https://adventofcode.com/2020/day/9
+*/
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Point struct {
+	x int
+	y int
+}
+
+var inputFile = flag.String("input", "../inputs/day09.txt", "Relative path to input-file")
+
+func input2slice(data []string) [][]int {
+	var heights [][]int
+	for _, s := range data {
+		var tmp []int
+		for _, r := range strings.Split(s, "") {
+			n, _ := strconv.Atoi(r)
+			tmp = append(tmp, n)
+		}
+		heights = append(heights, tmp)
+	}
+	return heights
+}
+
+func star1(data [][]int) []Point {
+	h := len(data)
+	w := len(data[0])
+	risk := 0
+	var lows []Point
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			var adj []int
+			pos := data[i][j]
+			if i > 0 {
+				adj = append(adj, data[i-1][j])
+			}
+			if i+1 < h {
+				adj = append(adj, data[i+1][j])
+			}
+			if j > 0 {
+				adj = append(adj, data[i][j-1])
+			}
+			if j+1 < w {
+				adj = append(adj, data[i][j+1])
+			}
+
+			lp := true
+			for _, n := range adj {
+				if pos >= n {
+					lp = false
+					break
+				}
+			}
+			if lp {
+				risk += pos + 1
+				lows = append(lows, Point{i, j})
+			}
+		}
+	}
+	fmt.Println("Star 1:", risk)
+	return lows
+}
+
+func star2(data [][]int, lows []Point) {
+	h := len(data)
+	w := len(data[0])
+	var basins []int
+	for _, p := range lows {
+		basin := make(map[Point]bool)
+		var points []Point
+		basin[p] = true
+		points = append(points, p)
+		if p.x > 0 {
+			np := Point{p.x - 1, p.y}
+			if data[np.x][np.y] < 9 {
+				basin[np] = true
+				points = append(points, np)
+			}
+		}
+		if p.x+1 < h {
+			np := Point{p.x + 1, p.y}
+			if data[np.x][np.y] < 9 {
+				basin[np] = true
+				points = append(points, np)
+			}
+		}
+		if p.y > 0 {
+			np := Point{p.x, p.y - 1}
+			if data[np.x][np.y] < 9 {
+				basin[np] = true
+				points = append(points, np)
+			}
+		}
+		if p.y+1 < w {
+			np := Point{p.x, p.y + 1}
+			if data[np.x][np.y] < 9 {
+				basin[np] = true
+				points = append(points, np)
+			}
+		}
+		i := 1
+		for i < len(points) {
+			pt := points[i]
+			if pt.x > 0 {
+				np := Point{pt.x - 1, pt.y}
+				if !basin[np] && data[np.x][np.y] < 9 {
+					basin[np] = true
+					points = append(points, np)
+				}
+			}
+			if pt.x+1 < h {
+				np := Point{pt.x + 1, pt.y}
+				if !basin[np] && data[np.x][np.y] < 9 {
+					basin[np] = true
+					points = append(points, np)
+				}
+			}
+			if pt.y > 0 {
+				np := Point{pt.x, pt.y - 1}
+				if !basin[np] && data[np.x][np.y] < 9 {
+					basin[np] = true
+					points = append(points, np)
+				}
+			}
+			if pt.y+1 < w {
+				np := Point{pt.x, pt.y + 1}
+				if !basin[np] && data[np.x][np.y] < 9 {
+					basin[np] = true
+					points = append(points, np)
+				}
+			}
+			i++
+		}
+		basins = append(basins, len(points))
+	}
+	sort.Ints(basins)
+	fmt.Println("Star 2:", basins[len(basins)-1]*basins[len(basins)-2]*basins[len(basins)-3])
+}
+
+func main() {
+	flag.Parse()
+	raw, err := ioutil.ReadFile(*inputFile)
+	if err != nil {
+		panic("Invalid file")
+	}
+	rawStr := strings.TrimSpace(string(raw))
+	data := strings.Split(rawStr, "\n")
+	input := input2slice(data)
+	lows := star1(input)
+	star2(input, lows)
+}
