b3981a4837364138bf1cb0bdca9f415a37b6e21c
Jakob Markgren <markgren@outlook.com>
2021-12-14 23:37:29 +0100

    [2021:Day14] Part two

--- 2021/go/day14.go
+++ 2021/go/day14.go
@@ -1,7 +1,7 @@
 package main
 
 /*
-Day 14: Title
+Day 14: Extended Polymerization
 
 https://adventofcode.com/2021/day/14
 */
@@ -15,27 +15,7 @@ import (
 
 var inputFile = flag.String("input", "../inputs/day14.txt", "Relative path to input-file")
 
-func runRules(pairIns []string, template string) string {
-	newTmp := ""
-	for i, r := range template[:len(template)-1] {
-		found := false
-		for _, pi := range pairIns {
-			AB := strings.Split(pi, " -> ")
-			if string(template[i:i+2]) == AB[0] {
-				newTmp += string(r) + AB[1]
-				found = true
-				break
-			}
-		}
-		if !found {
-			newTmp += string(r)
-		}
-	}
-	newTmp += string(template[len(template)-1])
-	return newTmp
-}
-
-func runRules2(pairIns []string, template string) string {
+func runRules(pairIns []string, template string) (string, int) {
 	newTmp := ""
 	var letters string
 	for _, r := range template {
@@ -51,13 +31,12 @@ func runRules2(pairIns []string, template string) string {
 			letters += AB[1]
 		}
 	}
-	fmt.Println(letters)
+
 	for i, r := range template[:len(template)-1] {
 		found := false
-		for _, pi := range pairIns {
-			AB := strings.Split(pi, " -> ")
-			if string(template[i:i+2]) == AB[0] {
-				newTmp += string(r) + AB[1]
+		for k, v := range rules {
+			if string(template[i:i+2]) == k {
+				newTmp += string(r) + v
 				found = true
 				break
 			}
@@ -67,49 +46,85 @@ func runRules2(pairIns []string, template string) string {
 		}
 	}
 	newTmp += string(template[len(template)-1])
-	return newTmp
+	max := 0
+	min := len(newTmp)
+	for _, r := range letters {
+		n := strings.Count(newTmp, string(r))
+		if n > max {
+			max = n
+		}
+		if n < min {
+			min = n
+		}
+	}
+	return newTmp, max - min
+}
+
+func string2pairs(s string) map[string]int {
+	pairs := make(map[string]int)
+	for i, _ := range s[:len(s)-1] {
+		pairs[s[i:i+2]]++
+	}
+	return pairs
+}
+func strings2rules(pairIns []string) map[string]string {
+	rules := make(map[string]string)
+	for _, pi := range pairIns {
+		AB := strings.Split(pi, " -> ")
+		rules[AB[0]] = AB[1]
+	}
+	return rules
+}
+
+func runRules2(pairs map[string]int, rules map[string]string) map[string]int {
+	new := make(map[string]int)
+	for k, v := range rules {
+		if pairs[k] > 0 {
+			a := string(k[0]) + v
+			b := v + string(k[1])
+			//fmt.Println(a, b, k, v)
+			new[a] += pairs[k]
+			new[b] += pairs[k]
+		}
+	}
+	return new
 }
 
 func star1(pairIns []string, template string) {
+	n := 0
 	for i := 0; i < 10; i++ {
-		template = runRules(pairIns, template)
+		template, n = runRules(pairIns, template)
 	}
-	rN := make(map[rune]int)
-	for _, r := range template {
-		rN[r]++
-	}
-	max := 0
-	min := len(template)
-	for _, v := range rN {
-		if v > max {
-			max = v
-		}
-		if v < min {
-			min = v
-		}
-	}
-	fmt.Println("Star 1:", max-min)
+	fmt.Println("Star 1:", n)
 }
 
 func star2(pairIns []string, template string) {
-	for i := 0; i < 10; i++ {
-		template = runRules2(pairIns, template)
+	first := string(template[0])
+	last := string(template[len(template)-1])
+	temp := string2pairs(template)
+	rules := strings2rules(pairIns)
+	for i := 0; i < 40; i++ {
+		temp = runRules2(temp, rules)
 	}
-	rN := make(map[rune]int)
-	/*for _, r := range template {
-		rN[r]++
+	nR := make(map[string]int)
+	// all letters are counted twice except first and last
+	nR[first] = 1
+	nR[last] = 1
+	for k, v := range temp {
+		nR[string(k[0])] += v
+		nR[string(k[1])] += v
 	}
 	max := 0
-	min := len(template)
-	for _, v := range rN {
-		if v > max {
-			max = v
+	min := nR[first]
+	for _, v := range nR {
+		if v/2 > max {
+			max = v / 2
 		}
-		if v < min {
-			min = v
+		if v/2 < min {
+			min = v / 2
 		}
-	}*/
-	fmt.Println("Star 2:", rN)
+	}
+	fmt.Println("Star 2:", max-min)
 }
 
 func main() {
