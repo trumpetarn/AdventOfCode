a53c94dd451ab8b91eefb111ae6e5ef4c19f7420
Jakob Markgren <jakob.markgren@gmail.com>
2022-12-11 01:09:12 +0100

    [2022:Day9] Part 1, movement not working for part 2

--- 2022/day09/day09.go
+++ 2022/day09/day09.go
@@ -1,36 +1,130 @@
+package main
 
-    package main
+/*
+   Day 09: https://adventofcode.com/2022/day/9
+*/
 
-    /*
-    Day 09: https://adventofcode.com/2022/day/9
-    */
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
-    import (
-        "flag"
-        "fmt"
-        "io/ioutil"
-        "strings"
-    )
+var inputFile = flag.String("input", "../inputs/day09.txt", "Relative path to input-file")
 
-    var inputFile = flag.String("input", "../inputs/day09.txt", "Relative path to input-file")
-    
-    func star1(data []string) {
-        fmt.Println("Star 1:", data[0])
-    }
+type Pos struct {
+	x int
+	y int
+}
 
-    func star2(data []string) {
-        fmt.Println("Star 2:", data[0])
-    }
+func isAdjacent(P1, P2 Pos) bool {
+	if P1.x == P2.x || P1.x+1 == P2.x || P1.x-1 == P2.x {
+		if P1.y == P2.y || P1.y+1 == P2.y || P1.y-1 == P2.y {
+			return true
+		}
+	}
+	return false
+}
 
-    func main() {
-        flag.Parse()
-        raw, err := ioutil.ReadFile(*inputFile)
-        if err != nil {
-            panic("Invalid file")
-        }
-        rawStr := strings.TrimSpace(string(raw))
-        data := strings.Split(rawStr, "\n\n")
-        star1(data)
-        star2(data)
-    }
-    
+func star1(data []string) {
+	var dir rune
+	var l int
+	H := Pos{0, 0}
+	T := Pos{0, 0}
+	history := make(map[Pos]bool)
+	history[T] = true
+	for _, ln := range data {
+		fmt.Sscanf(ln, "%c %d", &dir, &l)
+		for i := 0; i < l; i++ {
+			prevH := Pos{H.x, H.y}
+			switch dir {
+			case 'U':
+				H.y++
+			case 'R':
+				H.x++
+			case 'L':
+				H.x--
+			case 'D':
+				H.y--
+			default:
+				panic("Unkown direction")
+			}
+			if !isAdjacent(T, H) {
+				T = prevH
+				history[T] = true
+			}
+		}
+	}
+	fmt.Println("Star 1:", len(history))
+}
+
+func star2(data []string) {
+	var dir rune
+	var l int
+	rope := make([]Pos, 2)
+	for i := 0; i < len(rope); i++ {
+		rope[i] = Pos{0, 0}
+	}
+	history := make(map[Pos]bool)
+	history[rope[len(rope)-1]] = true
+	for _, ln := range data {
+		fmt.Sscanf(ln, "%c %d", &dir, &l)
+		for i := 0; i < l; i++ {
+			switch dir {
+			case 'U':
+				rope[0].y++
+			case 'R':
+				rope[0].x++
+			case 'L':
+				rope[0].x--
+			case 'D':
+				rope[0].y--
+			default:
+				panic("Unkown direction")
+			}
+
+			for j := 1; j < len(rope); j++ {
+				if rope[j-1].x+2 == rope[j].x && rope[j-1].y == rope[j].y {
+					rope[j].x++
+				} else if rope[j-1].x-2 == rope[j].x && rope[j-1].y == rope[j].y {
+					rope[j].x--
+				} else if rope[j-1].y+2 == rope[j].y && rope[j-1].x == rope[j].x {
+					rope[j].y++
+				} else if rope[j-1].y-2 == rope[j].y && rope[j-1].x == rope[j].x {
+					rope[j].y--
+				} else if !isAdjacent(rope[j], rope[j-1]) {
+					//diagonal
+					if rope[j-1].x < rope[j].x {
+						rope[j].x--
+					} else {
+						rope[j].x++
+					}
+					if rope[j-1].y > rope[j].y-1 {
+						rope[j].y--
+					} else {
+						rope[j].y++
+					}
+				}
+
+			}
+			//fmt.Println("  ", rope)
+			history[rope[len(rope)-1]] = true
+		}
+		//fmt.Println(rope)
+	}
+
+	fmt.Println("Star 2:", len(history))
+}
+
+func main() {
+	flag.Parse()
+	raw, err := ioutil.ReadFile(*inputFile)
+	if err != nil {
+		panic("Invalid file")
+	}
+	rawStr := strings.TrimSpace(string(raw))
+	data := strings.Split(rawStr, "\n")
+	star1(data)
+	star2(data)
+}
