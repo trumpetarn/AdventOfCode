7a44805893328703ec63fa95ed2748aebd20eb3e
Jakob Markgren <jakob.markgren@gmail.com>
2024-12-05 22:08:01 +0100

    [2024:Day2] Full solution

--- 2024/day02/day02.py
+++ 2024/day02/day02.py
@@ -27,23 +27,42 @@ def checkRules(l, max=3):
     return True
         
 
-def star1(file):
-    N = 0
-    for line in file:
+def star1(lists):
+    N1_safe = 0
+    N2_safe = 0
+    for l in lists:
+        if checkRules(l):
+            N1_safe += 1
+        else:
+            for i in range(len(l)):
+                print(i)
+                if checkRules(l[:i] + l[i+1:]):
+                    N2_safe += 1
+                    break
+    return N1_safe, N1_safe+N2_safe
+
+def star2(lists):
+    N_safe = 0
+    for l in lists:
+        if checkRules(l):
+            N_safe += 1
+        else:
+            for i in range(len(l)):
+                if checkRules(l[:i] + l[i+1:]):
+                    N_safe += 1
+                    break
+    return N_safe
+
+def main():
+    day2input = open("/repo/AdventOfCode/2024/inputs/day02.txt", 'r')
+    lists = []
+    for line in day2input:
         values = line.strip('\n').split(' ')
         l = [int(s) for s in values]
-        if checkRules(l):
-            N += 1
-    return N
-
-def star2(file):
-    return file
-
-def __main__():
-    day2input = open("/repo/AdventOfCode/2024/inputs/day02.txt", 'r')
-    ans1 = star1(day2input)
-    ans2 = star2(day2input)
+        lists.append(l)
+    ans1, ans2 = star1(lists)
     print("Star 1:", ans1)
     print("Star 2:", ans2)
 
-__main__()
+if __name__ == "__main__":
+    main()
