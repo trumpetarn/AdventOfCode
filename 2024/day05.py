27cf3fdf52ca4fb7cf58e15c7bd49478e1b4391c
Jakob Markgren <jakob.markgren@gmail.com>
2024-12-07 01:03:44 +0100

    [2024:day05] Cleanup

--- 2024/day05.py
+++ 2024/day05.py
@@ -23,31 +23,16 @@ def star1(rules, updates):
             ruleDict[r[0]] = [r[1]]
     for line in updates:
         order = line.split(",")
+        ordered = []
         inOrder = True
         for i, v in enumerate(order):
             r = ruleDict.get(v)
             if r != None:
                 s1 = set(r)
                 s2 = set(order[:i])
-                if len(s1.intersection(s2)) > 0:
-                    inOrder = False
-                    break
-        if inOrder:
-            res += int(order[(len(order) - 1) // 2])
-        else:
-            res2 += star2(order, ruleDict)
-    return res, res2
-
-
-def star2(order, ruleDict):
-    ordered = []
-    for i, v in enumerate(order):
-        r = ruleDict.get(v)
-        if r != None:
-            s1 = set(r)
-            s2 = set(order[:i])
-            inter = s1.intersection(s2)
+                inter = s1.intersection(s2)
             if len(inter) > 0:
+                inOrder = False
                 lowest = 9999999
                 for k in inter:
                     id = ordered.index(k)
@@ -58,7 +43,11 @@ def star2(order, ruleDict):
                 ordered.append(v)
         else:
             ordered.append(v)
-    return int(ordered[(len(ordered) - 1) // 2])
+        if inOrder:
+            res += int(order[(len(order) - 1) // 2])
+        else:
+            res2 += int(ordered[(len(ordered) - 1) // 2])
+    return res, res2
 
 
 def main():
