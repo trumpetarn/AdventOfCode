9e64c6bc1275a86b4b872c1ddc6f4762e6bcfb3e
Jakob Markgren <jakob.markgren@gmail.com>
2024-12-07 21:58:22 +0100

    [2024:day06] Some performace improvements but still slow

--- 2024/day06.py
+++ 2024/day06.py
@@ -4,14 +4,19 @@ Day 06
 
 from typing import List, Tuple, Dict, Set, Optional
 from pathlib import Path
-import copy
+from tqdm import tqdm
+from enum import IntEnum
 
-UP = "^"
-DOWN = "v"
-LEFT = "<"
-RIGHT = ">"
-DIR = [UP, DOWN, LEFT, RIGHT]
+
+class Direction(IntEnum):
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+
+
 OBSTRUCTION = "#"
+CLEAR = "."
 
 
 def read_input(file_path: str = "inputs/day06.txt") -> List[str]:
@@ -22,36 +27,33 @@ def read_input(file_path: str = "inputs/day06.txt") -> List[str]:
 def find_guard(input):
     for i, line in enumerate(input):
         for j, c in enumerate(line):
-            if c in DIR:
+            if c == "^":
                 return (i, j)
 
 
+move_map = {
+    Direction.UP: (-1, 0),
+    Direction.RIGHT: (0, 1),
+    Direction.DOWN: (1, 0),
+    Direction.LEFT: (0, -1),
+}
+
+
 def get_next(dir: str, pos: Tuple) -> Tuple:
-    if dir == UP:
-        return (pos[0] - 1, pos[1])
-    elif dir == DOWN:
-        return (pos[0] + 1, pos[1])
-    elif dir == LEFT:
-        return (pos[0], pos[1] - 1)
-    elif dir == RIGHT:
-        return (pos[0], pos[1] + 1)
-    else:
-        print("get_next(): ERROR, this should not happen", dir)
-    return (-1, -1)
+    move = move_map[dir]
+    return (pos[0] + move[0], pos[1] + move[1])
+
+
+turn_map = {
+    Direction.UP: Direction.RIGHT,
+    Direction.RIGHT: Direction.DOWN,
+    Direction.DOWN: Direction.LEFT,
+    Direction.LEFT: Direction.UP,
+}
 
 
 def turn(dir):
-    if dir == UP:
-        return RIGHT
-    elif dir == DOWN:
-        return LEFT
-    elif dir == LEFT:
-        return UP
-    elif dir == RIGHT:
-        return DOWN
-    else:
-        print("turn(): ERROR, this should not happen")
-    return "Q"
+    return turn_map[dir]
 
 
 def is_inside(pos, R, C):
@@ -60,7 +62,7 @@ def is_inside(pos, R, C):
 
 def move(dir, pos, input):
     next = get_next(dir, pos)
-    if input[next[0]][next[1]] == ".":
+    if input[next[0]][next[1]] == CLEAR:
         pos = next
         return next, dir
     elif input[next[0]][next[1]] == OBSTRUCTION:
@@ -81,7 +83,7 @@ def star1(input):
     COLS = len(input[0])
     visited = set()
     visited.add((pos))
-    dir = UP
+    dir = Direction.UP
     while is_inside(get_next(dir, pos), ROWS, COLS):
         next, dir = move(dir, pos, input)
         pos = next
@@ -102,20 +104,20 @@ def can_loop(input, pos, dir):
     while is_inside(get_next(dir, pos), ROWS, COLS):
         next, dir = move(dir, pos, input)
         if (next, dir) in visited:
-            input[orig_pos[0]][orig_pos[1]] = "."
+            input[orig_pos[0]][orig_pos[1]] = CLEAR
             return True
         pos = next
         visited.add((pos, dir))
-    input[orig_pos[0]][orig_pos[1]] = "."
+    input[orig_pos[0]][orig_pos[1]] = CLEAR
     return False
 
 
 def star2(input, visited):
     start_pos = find_guard(input)
     pos = start_pos
-    dir = UP
+    dir = Direction.UP
     loop = 0
-    for pos in visited:
+    for pos in tqdm(visited):
         if pos == start_pos:
             continue
         if can_loop(input, pos, dir):
