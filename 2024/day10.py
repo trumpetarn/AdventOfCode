831748caf0c4f24eaa586a5c53a45c2424796be7
Jakob Markgren <jakob.markgren@gmail.com>
2024-12-10 23:55:09 +0100

    [2024:day10] Cleanup and fancyfying

--- 2024/day10.py
+++ 2024/day10.py
@@ -7,30 +7,12 @@ from pathlib import Path
 import itertools
 
 
-def read_input(file_path: str = "inputs/day10.txt") -> List[str]:
+def read_input(file_path: str = "inputs/day10.txt") -> List[List[int]]:
     f = Path(file_path).read_text().strip().split("\n")
-    return f
+    return [[int(v) for v in line] for line in f]
 
 
-def parse_input(input):
-    m = []
-    for line in input:
-        l = []
-        for v in line:
-            l.append(int(v))
-        m.append(l)
-    return m
-
-
-def get_next_diag(top_map, x, y):
-    R = len(top_map)
-    C = len(top_map[0])
-    xx = list(range(max(0, x - 1), min(x + 2, C)))
-    yy = list(range(max(0, y - 1), min(y + 2, R)))
-    return list(itertools.product(xx, yy))
-
-
-def get_next(top_map, x, y, N):
+def get_next(top_map: List[List[int]], x: int, y: int, N: int) -> List[Tuple]:
     R = len(top_map)
     C = len(top_map[0])
     next = [
@@ -42,7 +24,7 @@ def get_next(top_map, x, y, N):
     return [p for p in next if p != (x, y) and top_map[p[0]][p[1]] == N]
 
 
-def compute_score(top_map, start_x, start_y):
+def compute_path(top_map: List[List[int]], start_x: int, start_y: int) -> List[Tuple]:
     current_pos = [(start_x, start_y)]
     for i in range(1, 10):
         next_pos = []
@@ -50,49 +32,24 @@ def compute_score(top_map, start_x, start_y):
             next_pos += get_next(top_map, x, y, i)
         current_pos = next_pos
         if len(current_pos) == 0:
-            break
+            return []
 
-    return len(set(current_pos))
-
-
-def compute_score2(top_map, start_x, start_y):
-    current_pos = [(start_x, start_y)]
-    for i in range(1, 10):
-        next_pos = []
-        for x, y in current_pos:
-            next_pos += get_next(top_map, x, y, i)
-        current_pos = next_pos
-        if len(current_pos) == 0:
-            break
-
-    return len((current_pos))
-
-
-def star1(top_map):
-    score = 0
-    for i, line in enumerate(top_map):
-        for j, p in enumerate(line):
-            if p == 0:
-                score += compute_score(top_map, i, j)
-    return score
-
-
-def star2(top_map):
-    score = 0
-    for i, line in enumerate(top_map):
-        for j, p in enumerate(line):
-            if p == 0:
-                score += compute_score2(top_map, i, j)
-    return score
+    return current_pos
 
 
 def main():
-    input = read_input()
-    top_map = parse_input(input)
-    ans1 = star1(top_map)
-    ans2 = star2(top_map)
-    print("Star 1:", ans1)
-    print("Star 2:", ans2)
+    top_map = read_input()
+    score = 0
+    rating = 0
+    for i, line in enumerate(top_map):
+        for j, p in enumerate(line):
+            if p == 0:
+                p = compute_path(top_map, i, j)
+                score += len(set(p))
+                rating += len(p)
+
+    print("Star 1:", score)
+    print("Star 2:", rating)
 
 
 if __name__ == "__main__":
