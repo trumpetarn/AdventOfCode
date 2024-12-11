767433873251ac9212fed0e2c5a14d9634cf5c58
Jakob Markgren <jakob.markgren@gmail.com>
2024-12-11 23:34:45 +0100

    [2024:day11] Improved and working

--- 2024/day11.py
+++ 2024/day11.py
@@ -1,45 +1,41 @@
 from tqdm import tqdm
 
 
-def star1(N):
-    # input = [125, 17]
+def stones(N):
+    input = [125, 17]
     input = [4329, 385, 0, 1444386, 600463, 19, 1, 56615]
-    seen = set()
-    hist = {}
+    data = dict()
     for v in input:
-        seen.add(v)
-        hist[v] = [0]
-
-    for i in range(N):
-        new_input = []
-        for x in input:
-            if x == 0:
-                new_input.append(1)
-            else:
-                sx = str(x)
-                if len(sx) % 2 == 0:
-                    new_input.append(int(sx[: len(sx) // 2]))
-                    new_input.append(int(sx[len(sx) // 2 :]))
+        data[v] = 1
+    for i in tqdm(range(N)):
+        new = []
+        for x in data:
+            if data[x] > 0:
+                n = data[x]
+                data[x] = 0
+                if x == 0:
+                    new += [(1, n)]
                 else:
-                    new_input.append(x * 2024)
-        input = new_input
-    #        input = []
-    #        for v in new_input:
-    #            if v in seen:
-    #                hist[v].append(i)
-    #            else:
-    #                input.append(v)
-    #                hist[v] = [i]
-    #        seen.update(new_input)
+                    sx = str(x)
+                    if len(sx) % 2 == 0:
+                        a = int(sx[: len(sx) // 2])
+                        b = int(sx[len(sx) // 2 :])
+                        new += [(a, n), (b, n)]
+                    else:
+                        new += [(x * 2024, n)]
 
-    print(hist)
-    return len(new_input)
+        for v, n in new:
+            data[v] = data.get(v, 0) + n
+
+    return data
 
 
 def main():
+    a = stones(25)
+    b = stones(75)
 
-    print("Star 1:", star1(6))
-    print("Star 2:", 0)
+    print("Star 1:", sum([x for x in a.values()]))
+    print("Star 2:", sum([x for x in b.values()]))
 
 
 if __name__ == "__main__":
