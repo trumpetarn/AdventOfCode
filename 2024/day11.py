5a63c4fad065009cbc5626ec4d88fb0f116bba7a
Jakob Markgren <jakob.markgren@gmail.com>
2024-12-11 21:02:03 +0100

    [2024:day11] WIP, repeating values is the key?

--- 2024/day11.py
+++ 2024/day11.py
@@ -2,28 +2,43 @@ from tqdm import tqdm
 
 
 def star1(N):
-    input = [125, 17]
-    # input = [4329, 385, 0, 1444386, 600463, 19, 1, 56615]
+    # input = [125, 17]
+    input = [4329, 385, 0, 1444386, 600463, 19, 1, 56615]
+    seen = set()
+    hist = {}
+    for v in input:
+        seen.add(v)
+        hist[v] = [0]
 
-    for _ in range(N):
+    for i in range(N):
         new_input = []
-        for i in input:
-            if i == 0:
+        for x in input:
+            if x == 0:
                 new_input.append(1)
             else:
-                si = str(i)
-                if len(si) % 2 == 0:
-                    new_input.append(int(si[: len(si) // 2]))
-                    new_input.append(int(si[len(si) // 2 :]))
+                sx = str(x)
+                if len(sx) % 2 == 0:
+                    new_input.append(int(sx[: len(sx) // 2]))
+                    new_input.append(int(sx[len(sx) // 2 :]))
                 else:
-                    new_input.append(i * 2024)
+                    new_input.append(x * 2024)
         input = new_input
+    #        input = []
+    #        for v in new_input:
+    #            if v in seen:
+    #                hist[v].append(i)
+    #            else:
+    #                input.append(v)
+    #                hist[v] = [i]
+    #        seen.update(new_input)
+
+    print(hist)
     return len(new_input)
 
 
 def main():
 
-    print("Star 1:", star1(25))
+    print("Star 1:", star1(6))
     print("Star 2:", 0)
 
 
