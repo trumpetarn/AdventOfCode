d60290f37644ef900923b8325b54fbce998ed00b
Jakob <jakob.markgren@sartorius.com>
2024-12-26 19:41:42 +0100

    [2024:day21] wip p2

--- 2024/day21.py
+++ 2024/day21.py
@@ -6,6 +6,7 @@ https://adventofcode.com/2024/day/21
 
 from typing import List, Tuple, Dict, Set, Optional
 from pathlib import Path
+from tqdm import tqdm
 """
 +---+---+---+
 | 7 | 8 | 9 |
@@ -27,7 +28,10 @@ NPAD = [['7', '8', '9'],['4', '5', '6'],['1', '2', '3'], ['', '0', 'A']]
 +---+---+---+
 """  
 ARROW = [['', '^', 'A'],['<', 'v', '>']]
+cache = {}
 def shortest_path(p1, p2, keys=NPAD) -> str:
+    if keys == ARROW and (p1,p2) in cache:
+        return cache[(p1,p2)]
     if p1 == p2:
         return "A"
     for i, row in enumerate(keys):
@@ -65,7 +69,8 @@ def shortest_path(p1, p2, keys=NPAD) -> str:
             s += ">"*abs(r1[1]-r2[1])
         if r1[0] > r2[0]:
             s += "^"*abs(r1[0]-r2[0])
-        
+    if keys == ARROW:
+        cache[(p1,p2)] = s + "A"
     return s+"A"
 
 
@@ -73,35 +78,13 @@ def read_input(file_path: str = "./inputs/day21.in") -> List[str]:
     f = Path(file_path).read_text().strip().split("\n")
     return f
 
-
-def star1(input):
-    answers = []
-    for p in input:
-        cmds = shortest_path('A', p[0])
-        for i,n in enumerate(p[:-1]):
-            cmds += shortest_path(n, p[i+1])
-        
-        # Robot 2
-        cmd2 = shortest_path('A', cmds[0], ARROW)
-        for i,n in enumerate(cmds[:-1]):
-            cmd2 += shortest_path(n, cmds[i+1], ARROW)
-        
-        # Robot 3
-        cmd3 = shortest_path('A', cmd2[0], ARROW)
-        for i,n in enumerate(cmd2[:-1]):
-            cmd3 += shortest_path(n, cmd2[i+1], ARROW)
-        answers.append(len(cmd3)*int(p[:-1]))
-    print(answers)
-    return sum(answers)
-
-
-def star2(input):
+def stars(input, npad_robots = 2):
     ans = 0
     for p in input:
         cmds = shortest_path('A', p[0])
         for i,n in enumerate(p[:-1]):
             cmds += shortest_path(n, p[i+1])
-        for j in range(0,2):
+        for _ in range(npad_robots):
             cmd2 = shortest_path('A', cmds[0], ARROW)
             for i,n in enumerate(cmds[:-1]):
                 cmd2 += shortest_path(n, cmds[i+1], ARROW)
@@ -113,9 +96,13 @@ def star2(input):
 
 def main():
     input = read_input()
-    ans1 = star1(input)
-    ans2 = star2(input)
+    for i in ["^", "v", "<", ">", "A"]:
+        for j in ["^", "v", "<", ">", "A"]:
+            shortest_path(i, j, ARROW)
+    print(cache)
+    ans1 = stars(input)
     print("Star 1:", ans1)
+    ans2 = stars(input, 25)
     print("Star 2:", ans2)
 
 
